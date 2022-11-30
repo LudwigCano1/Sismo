29b576417cace911e85b009aa289025f4e9e2ea2
LudwigCano1 <115447154+LudwigCano1@users.noreply.github.com>
2022-11-30 15:12:29 -0500

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from openpyxl import Workbook
+
+wb = Workbook()
+
+wb.create_sheet("Hoja_A",0)
+
+direc = st.text_input("Dirección:")
+filename = st.text_input("Nombre de archivo:")
+n = direc+"\\"+filename+".xlsx"
+print(n)
+
+if st.button("Descargar"):
+    wb.save(n)
