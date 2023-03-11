fa687d7069832ba9a57e9e1e8445b6f0ba583e7c
LudwigCano1 <115447154+LudwigCano1@users.noreply.github.com>
2023-03-11 16:37:15 -0500

    Create prueba2.py

---
+++ prueba2.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import imageio
+
+from io import BytesIO
+from tempfile import NamedTemporaryFile
+
+
+x = [1, 2, 3, 4, 4, 4, 4, 3, 2, 1, 1, 1, 1]
+y = [1, 1, 1, 1, 2, 3, 4, 4, 4, 4, 3, 2, 1]
+time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+def create_frame(t):
+    fig = plt.figure(figsize=(6, 6))
+    plt.plot(x[:(t+1)], y[:(t+1)], color = 'grey' )
+    plt.plot(x[t], y[t], color = 'black', marker = 'o' )
+    plt.xlim([0,5])
+    plt.xlabel('x', fontsize = 14)
+    plt.ylim([0,5])
+    plt.ylabel('y', fontsize = 14)
+    plt.title(f'Relationship between x and y at step {t}',
+              fontsize=14)
+    
+    with NamedTemporaryFile() as tmp:
+        plt.savefig(tmp.name, 
+                    transparent = False,  
+                    facecolor = 'white'
+                )
+        data = BytesIO(tmp.read())
+    plt.close()
+    return data
+
+frames = []
+for t in time:
+    frames.append(imageio.v2.imread(create_frame(t)))
+
+with NamedTemporaryFile() as tmp:
+    imageio.mimsave(tmp.name, # output gif
+                    frames,          # array of input frames
+                    fps = 5)         # optional: frames per second
+    data = BytesIO(tmp.read())
+
+st.image(data)
